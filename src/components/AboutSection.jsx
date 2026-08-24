32e2e75897bf227cd952b8e46e480560eeca14ac
Nishal Fernando <nishalfernando2006@gmail.com>
2026-08-24 09:50:49 +0530

    cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -49,8 +49,8 @@ export const AboutSection = () => {
                                 Get In Touch
                             </a>
                             <a
-                                href="./Nishal_Fernando_CV.pdf"
-                                download="Nishal_Fernando_CV.pdf"
+                                href="./Nishal Fernando CV.pdf"
+                                download="Nishal FernandoCV.pdf"
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             >
                                 Download CV
